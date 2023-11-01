23141ab9bbbd1a80b6d5b80ad20db936a0b4ee97
diogo-santos302 <diogoejsantos@ua.pt>
2023-11-01 13:53:48 +0000

    some refactoring on MainActivity

--- app/src/main/java/com/example/mydaylogger/MainActivity.kt
+++ app/src/main/java/com/example/mydaylogger/MainActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,19 +19,18 @@ import com.example.mydaylogger.location.GeofencingService
 import com.example.mydaylogger.ui.theme.MyDayLoggerTheme
 import com.example.mydaylogger.ui.ProfileScreen
 
-const val TAG = "MainActivity"
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     private lateinit var geofencingService: GeofencingService
     private lateinit var currentLocationService: CurrentLocationService
 
-    private val requestPermissionLauncher = registerForActivityResult(
+    private val notificationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
     ) { isGranted: Boolean ->
         if (isGranted) {
             Log.d(TAG, "FCM SDK (and your app) can post notifications.")
         } else {
-            // TODO: Inform user that that your app will not show notifications.
             Log.d(TAG, "Your app will not show notifications.")
         }
     }
@@ -39,18 +39,21 @@ class MainActivity : ComponentActivity() {
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
         when {
-//                permissions.getOrDefault(Manifest.permission.ACCESS_BACKGROUND_LOCATION, false) -> {
-//                    Log.i(TAG, "Background location access granted.")
-//                    addGeofence(geofencingService)
-//                }
-            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                Log.i(TAG, "Precise location access granted.")
+            permissions.getOrDefault(Manifest.permission.ACCESS_BACKGROUND_LOCATION, false) -> {
+                Log.d(TAG, "Background location access granted.")
                 addGeofence(geofencingService)
+                currentLocationService.getCurrentLocationOrNull { location ->
+                    Log.i(TAG, "${location?.latitude}, ${location?.longitude}")
+                }
             }
-            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                Log.i(TAG, "Only approximate location access granted.")
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                Log.d(TAG, "Precise location access granted.")
+                addGeofence(geofencingService)
+                currentLocationService.getCurrentLocationOrNull { location ->
+                    Log.i(TAG, "${location?.latitude}, ${location?.longitude}")
+                }
             } else -> {
-                Log.i(TAG, "No location access granted.")
+                Log.d(TAG, "No location access granted.")
             }
         }
     }
@@ -88,42 +91,54 @@ class MainActivity : ComponentActivity() {
                 //       If the user selects "No thanks," allow the user to continue without notifications.
             } else {
                 // Directly ask for the permission
-                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                notificationPermissionRequest.launch(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
 
     private fun askLocationPermission() {
-        when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) -> {
-                // You can use the API that requires the permission.
-                Log.i(TAG, "You can use the API that requires the permission.")
-                addGeofence(geofencingService)
-                currentLocationService.getCurrentLocationOrNull { location ->
-                    Log.i(TAG, "${location?.latitude}, ${location?.longitude}")
-                }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            askBackgroundLocationPermission()
+        } else {
+            askFineLocationPermission()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun askBackgroundLocationPermission() {
+        val permission = Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        if (ContextCompat.checkSelfPermission(this, permission) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d(TAG, "You can use the API that requires the permission.")
+            addGeofence(geofencingService)
+            currentLocationService.getCurrentLocationOrNull { location ->
+                Log.i(TAG, "${location?.latitude}, ${location?.longitude}")
             }
-            //            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-            //                // In an educational UI, explain to the user why your app requires this
-            //                // permission for a specific feature to behave as expected, and what
-            //                // features are disabled if it's declined. In this UI, include a
-            //                // "cancel" or "no thanks" button that lets the user continue
-            //                // using your app without granting the permission.
-            //                showInContextUI()
-            //            }
-            else -> {
-                // You can directly ask for the permission.
-                // The registered ActivityResultCallback gets the result of this request.
-                locationPermissionRequest.launch(
-                    arrayOf(
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    )
-                )
+        } else if (shouldShowRequestPermissionRationale(permission)) {
+            // TODO: display an educational UI explaining to the user the features that will be enabled
+            //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+            //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+            //       If the user selects "No thanks," allow the user to continue without notifications.
+        } else {
+            locationPermissionRequest.launch(arrayOf(permission))
+        }
+    }
+
+    private fun askFineLocationPermission() {
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        if (ContextCompat.checkSelfPermission(this, permission) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d(TAG, "You can use the API that requires the permission.")
+            addGeofence(geofencingService)
+            currentLocationService.getCurrentLocationOrNull { location ->
+                Log.i(TAG, "${location?.latitude}, ${location?.longitude}")
             }
+        } else {
+            locationPermissionRequest.launch(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, permission)
+            )
         }
     }
 
