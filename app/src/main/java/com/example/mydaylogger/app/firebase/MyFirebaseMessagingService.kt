20d4e90df954eaeeb563ba3e8e93b1e172aa7e13
diogo-santos302 <diogoejsantos@ua.pt>
2023-11-07 12:34:27 +0000

    foreground notifications

--- app/src/main/java/com/example/mydaylogger/app/firebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/mydaylogger/app/firebase/MyFirebaseMessagingService.kt
@@ -1,28 +1,51 @@
 package com.example.mydaylogger.app.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.mydaylogger.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 private const val TAG = "MyFirebaseMessagingService"
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
+    private val channelId = "Default"
+    private lateinit var notificationManager: NotificationManager
+
+    companion object {
+        private var notificationId = 1
+            get() = field++
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationChannel = NotificationChannel(
+            channelId,
+            "Default channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(notificationChannel);
+    }
+
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: ${remoteMessage.from}")
 
-        // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "Message data payload: ${remoteMessage.data}")
             remoteMessage.data["latitude"]?.let { Log.d(TAG, it) }
             remoteMessage.data["longitude"]?.let { Log.d(TAG, it) }
             remoteMessage.data["radiusInMetres"]?.let { Log.d(TAG, it) }
 
+            // TODO: Geofencing
             // Check if data needs to be processed by long running job
 //            if (needsToBeScheduled()) {
 //                // For long-running tasks (10 seconds or more) use WorkManager.
@@ -33,12 +56,14 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 //            }
         }
 
-        // Check if message contains a notification payload.
         remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
+            val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(it.title)
+                .setContentText(it.body)
+                .setAutoCancel(true)
+            notificationManager.notify(notificationId, notificationBuilder.build())
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 }
