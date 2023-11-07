9b41f619f21d0bd3be0f55a4024b40553147168d
DudaArinvs <eduarda.arvins@ua.pt>
2023-11-07 11:46:56 +0000

    top bar Mayday

--- app/src/main/java/com/example/mydaylogger/app/screens/Mayday.kt
+++ app/src/main/java/com/example/mydaylogger/app/screens/Mayday.kt
@@ -1,10 +1,13 @@
 package com.example.mydaylogger.app.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
@@ -12,11 +15,27 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun MaydayScreen(){
+
+    Column {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Mayday Page",
+                style = TextStyle(color = MaterialTheme.colors.onPrimary),
+                fontSize = 24.sp)
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+    )
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -27,6 +46,7 @@ fun MaydayScreen(){
             modifier = Modifier.padding(bottom = 8.dp),
             fontSize = 24.sp
         )
+        Spacer(modifier = Modifier.size(20.dp))
         Button(
             modifier = Modifier
                 .fillMaxWidth()
@@ -43,5 +63,6 @@ fun MaydayScreen(){
                 fontSize = 18.sp // Increase the font size of the text
             )
         }
+    }
     }
 }
